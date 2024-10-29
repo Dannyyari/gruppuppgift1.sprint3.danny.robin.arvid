67099d8dd19abe2f64b139d9f48530ae6fc0b49e
Danny Yari <danny_yari@yahoo.com>
2024-10-29 19:45:17 +0100

    uppbyggnad av metoder i Randomizedbutton

--- src/RandomizeButtons.java
+++ src/RandomizeButtons.java
@@ -1,24 +1,29 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomizeButtons {
     public RandomizeButtons(){}
 
-    public List<JButton> getRamdomizedButtons(){
+    public List<JButton> getRamdomizedButtons(List<JButton> listOfButtonsInOrder){
         List<JButton> random=new ArrayList<>();
-
-
-
+        for (JButton button : listOfButtonsInOrder) {
+            random.add(button);
+        }
+        Collections.shuffle(random);
         return random;
     }
 
-    //för att hämta int
-    public int getButtonNumber(){
+    //onödig lista kanske
+    //tar en JButton List och gör om till Integer List
+    public List<Integer> getButtonNumber(List<JButton> listOfButtonsAlreadyRandomized){
         int button=0;
+        List<Integer> returnButtonNumber=new ArrayList<>();
+        for (JButton buttonint : listOfButtonsAlreadyRandomized) {
+          returnButtonNumber.add(Integer.parseInt(buttonint.getText()));
+        }
 
-
-
-        return button;
+        return returnButtonNumber;
     }
 }
