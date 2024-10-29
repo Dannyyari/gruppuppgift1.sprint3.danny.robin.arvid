1c2b26b4ce050b954fa5029da980d5ab65a4eb7d
Danny Yari <danny_yari@yahoo.com>
2024-10-29 19:36:42 +0100

    skapande av klass och metod

---
+++ src/RandomizeButtons.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomizeButtons {
+    public RandomizeButtons(){}
+
+    public List<JButton> getRamdomizedButtons(){
+        List<JButton> random=new ArrayList<>();
+
+
+
+        return random;
+    }
+
+    //för att hämta int
+    public int getButtonNumber(){
+        int button=0;
+
+
+
+        return button;
+    }
+}
