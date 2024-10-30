078a20bcc6011c218e346b3dbc8c0c095ef5d0dc
Arvid Utas <arvidu@hotmail.com>
2024-10-30 14:53:18 +0100

    merged Arvids and Dannys branches

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main extends JFrame implements ActionListener {
-    RandomizeButtons rb =new RandomizeButtons();
+    RandomizeButtons rb = new RandomizeButtons();
 
-    JPanel mainArea =new JPanel();
- //   JPanel centerArea =new JPanel();
-    JPanel southArea=new JPanel();
-    JPanel playArea=new JPanel();
-    JButton resetButton=new JButton("Nytt spel");
+    JPanel mainArea = new JPanel();
+    JPanel southArea = new JPanel();
+    JPanel playArea = new JPanel();
+    JButton resetButton = new JButton("Nytt spel");
     JButton b1 = new JButton("1");
     JButton b2 = new JButton("2");
     JButton b3 = new JButton("3");
@@ -31,15 +29,15 @@ public class Main extends JFrame implements ActionListener {
     JButton b14 = new JButton("14");
     JButton b15 = new JButton("15");
     JButton b16 = new JButton("");
-    List<JButton> listOfButtonsSorted= Arrays.asList(b1, b2,b3,b4,b5,b6,b7,b8,
-            b9,b10,b11,b12,b13,b14,b15,b16);
-    List<JButton> listOfRandomizedButtons= rb.getRamdomizedButtons(listOfButtonsSorted);
+    List<JButton> listOfButtonsSorted = Arrays.asList(b1, b2, b3, b4, b5, b6, b7, b8,
+            b9, b10, b11, b12, b13, b14, b15, b16);
+    List<JButton> listOfRandomizedButtons = rb.getRamdomizedButtons(listOfButtonsSorted);
     JButton clickedButton;
 
-    public Main(){
+    public Main() {
         add(mainArea);
         mainArea.setLayout(new BorderLayout());
-        playArea.setLayout(new GridLayout(4,4));
+        playArea.setLayout(new GridLayout(4, 4));
         mainArea.add(southArea, BorderLayout.SOUTH);
         mainArea.add(playArea);
         southArea.add(resetButton);
@@ -66,26 +64,29 @@ public class Main extends JFrame implements ActionListener {
         b15.addActionListener(this);
 
         setTitle("Slide Game");
-        setSize(400,400);
+        setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // pack(); //ska göras på slutet
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==resetButton) {
-            List<JButton> randomizedListForButton=rb.getRamdomizedButtons(listOfButtonsSorted);
+        if (e.getSource() == resetButton) {
+            List<JButton> randomizedListForButton = rb.getRamdomizedButtons(listOfButtonsSorted);
             playArea.removeAll();
-            for (JButton jButton : randomizedListForButton){
+            for (JButton jButton : randomizedListForButton) {
                 playArea.add(jButton);
             }
             playArea.repaint();
             //funktion nedan förnyar rutan
             SwingUtilities.updateComponentTreeUI(this);
-            }
-        else {
+        } else {
             clickedButton = (JButton) e.getSource();
             moveButton();
         }
@@ -103,9 +104,5 @@ public class Main extends JFrame implements ActionListener {
             playArea.repaint();
             SwingUtilities.updateComponentTreeUI(this);
         }
-    }
-
-    public static void main(String[] args) {
-        Main m =new Main();
     }
 }
